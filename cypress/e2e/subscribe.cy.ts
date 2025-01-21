632d22736385d552b044718929702bb1410bd5bf
Nfoye Daryl <maviance.daryl.nfoye@gmail.com>
2025-01-21 13:35:20 +0100

    added selectors object to group selectors

--- cypress/e2e/subscribe.cy.ts
+++ cypress/e2e/subscribe.cy.ts
@@ -1,4 +1,10 @@
 const baseUrl =  Cypress.env("BASE_URL");
+const selectors = {
+    emailInput: "[data-test='email-input']",
+    submitButton: "[data-test='submit-button']",
+    successMessage: "[data-test='success-message']",
+    serverErrorMessage: "[data-test='server-error-message']"
+}
 
 describe("Test a form", () =>{
     beforeEach(()=>{
@@ -7,21 +13,24 @@ describe("Test a form", () =>{
 
     it("suscribe a user with valid email", ()=>{
         cy.fixture("test_data").then((data)=>{
-            cy.getByData("email-input").type(data.valid_email)
+            cy.get(selectors.emailInput).type(data.valid_email)
         })
-        cy.getByData("submit-button").click()
-        cy.getByData("success-message").should("exist")
+        cy.get(selectors.submitButton).click()
+        cy.get(selectors.successMessage).should("exist")
     })
     it("Doesn't suscribe a user with invalid email", ()=>{
-        cy.getByData("email-input").type("daryl")
-        cy.getByData("submit-button").click()
-        cy.getByData("success-message").should("not.exist")
+        cy.fixture("test_data").then((data)=>{
+            cy.get(selectors.emailInput).type(data.invalid_email)
+        })
+        cy.get(selectors.emailInput).type("daryl")
+        cy.get(selectors.submitButton).click()
+        cy.get(selectors.successMessage).should("not.exist")
     })
     it("Doesn't suscribe with an already existing email", ()=>{
         cy.fixture("test_data").then((data)=>{
-            cy.getByData("email-input").type(data.invalid_email)
+            cy.get(selectors.emailInput).type(data.existing_email)
         })
-        cy.getByData("submit-button").click()
-        cy.getByData("server-error-message").should("exist")
+        cy.get(selectors.submitButton).click()
+        cy.get(selectors.serverErrorMessage).should("exist")
     })
 })
--- cypress/e2e/user-journey.cy.ts
+++ cypress/e2e/user-journey.cy.ts
@@ -1,23 +1,31 @@
 const baseUrl =  Cypress.env("BASE_URL");
 
+const selectors = {
+    emailInput: "[data-test='email-input']",
+    submitButton: "[data-test='submit-button']",
+    successMessage: "[data-test='success-message']",
+    serverErrorMessage: "[data-test='server-error-message']",
+    course0: "[data-test='course-0']",
+    nextLessonButton: "[data-test='next-lesson-button']",
+    challengeAnswer0: "[data-test='challenge-answer-0']"
+}
 describe('User Journey', () => {
 
     beforeEach(() => {
         cy.visit(baseUrl)
     })
 
-    it("User can fin a course on the homepage and complete the course lessons",() => {
-        cy .getByData("course-0").find("a").contains("Get started").click()
+    it("User can find a course on the homepage and complete the course lessons",() => {
+        cy.get(selectors.course0).find("a").contains("Get started").click()
         cy.location("pathname").should("eq","/testing-your-first-application")
-        cy.getByData("next-lesson-button").click()
-        cy.getByData("challenge-answer-0").click()
-        cy.getByData("next-lesson-button").should("exist").click()
+        cy.get(selectors.nextLessonButton).click()
+        cy.get(selectors.challengeAnswer0).click()
+        cy.get(selectors.nextLessonButton).should("exist").click()
         cy.location("pathname").should("eq","/testing-your-first-application/installing-cypress-and-writing-our-first-test")
-        cy.getByData("challenge-answer-0").click()
-        cy.getByData("next-lesson-button").click()
+        cy.get(selectors.challengeAnswer0).click()
+        cy.get(selectors.nextLessonButton).click()
         cy.location("pathname").should("eq","/testing-your-first-application/setting-up-data-before-each-test")
-        cy.getByData("challenge-answer-0").click()
-        cy.getByData("next-lesson-button").click()
+        cy.get(selectors.challengeAnswer0).click()
+        cy.get(selectors.nextLessonButton).click()
     })
-}
-)
+})
